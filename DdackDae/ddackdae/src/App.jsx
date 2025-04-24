6028e40f060b57ebfa89c474881b45e2eb0cde5d
mining <whgpals641@naver.com>
2025-04-25 02:51:18 +0900

    사이드 수정 완료! 이걸로 작업해주세요.

--- DdackDae/ddackdae/src/App.jsx
+++ DdackDae/ddackdae/src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useRef } from 'react'
 import './App.css'
 import parkingClusters from './data/mockParkingCluster'
 import Sidebar from './components/Sidebar'
-import SideOpen from './components/SideOpen.jsx'
+
 
 
 function App() {
@@ -61,7 +61,7 @@ function App() {
   return (
     <div>
       <Sidebar /> {/* ✅ 지도 위에 사이드바 */}
-      <SideOpen />
+     
       <div
         ref={mapRef}
         id="map"
--- DdackDae/ddackdae/src/components/Sidebar.jsx
+++ DdackDae/ddackdae/src/components/Sidebar.jsx
@@ -1,34 +1,40 @@
-import '@/components/Sidebar.css'
-import logoImg from '@/assets/logo2.png'
-import { FaSearch, FaBookmark, FaUser } from 'react-icons/fa'
+import "@/components/Sidebar.css";
+import logo2 from "@/assets/logo2.png";
+import { FaSearch, FaBookmark, FaUser } from "react-icons/fa";
+import SideOpen from "@/components/Search/SideOpen";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="top">
-        <div className="logo">
-        <img src={logoImg} alt="로고" />
-        </div>
-        <div className="menu">
-          <div className="icon">
-            <FaSearch />
-            <span>검색</span>
+      <div className="sidebarNav">
+        <div className="top">
+          <div className="logo">
+            <img src={logo2} alt="로고" />
           </div>
+          <div className="menu">
+            <div className="icon">
+              <FaSearch />
+              <span>검색</span>
+            </div>
+            <div className="icon">
+              <FaBookmark />
+              <span>찜</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="bottom">
           <div className="icon">
-            <FaBookmark />
-            <span>찜</span>
+            <FaUser />
+            <span>로그인</span>
           </div>
         </div>
       </div>
-
-      <div className="bottom">
-        <div className="icon">
-          <FaUser />
-          <span>로그인</span>
-        </div>
+      <div className="sideOpen">
+        <SideOpen />
       </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
