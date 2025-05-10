dbf4797f60cec344bb08d21541b6a2e73331f69a
leetorrl <leetorrl123@gmail.com>
2025-05-10 22:35:51 +0900

    0510  병합후 라우터 기능 이슈 및 버튼 css 미적용 해결

--- DdackDae/ddackdae/src/components/Sidebar.jsx
+++ DdackDae/ddackdae/src/components/Sidebar.jsx
@@ -11,12 +11,18 @@ import SideOpen from "../components/search/SideOpen.jsx";
 import { useState } from "react";
 import LoginModal from "@/components/loginview/LoginModal.jsx";
 import SignupModal from "@/components/signupview/SignupModal.jsx";
+import ReviewModal from "@/components/review/ReviewModal.jsx"
+import EditUserModal from "@/components/edituserinformation/EditUserInformationModal.jsx";
+import UserInformationModal from "@/components/userinformation/UserInformationModal.jsx";
 
 function Sidebar() {
   const [open, setOpen] = useState(false);
   const [selectValue, setSelectValue] = useState("default");
   const [showModal, setShowModal] = useState(false);
   const [showSignModal, setSignModal] = useState(false);
+  const [showReviewModal, setReviewModal] = useState(false);
+  const [showEditUserModal, setEditUserModal] = useState(false);
+  const [showUserInformationModal, setUserInformationModal ] = useState(false);
 
   const handleBodySelect = (newKey) => {
     setSelectValue(newKey || "default");
@@ -42,6 +48,30 @@ function Sidebar() {
   const closeSingModal = () => {
     setSignModal(false);
   };
+
+  const Review = () => {
+    setReviewModal(true);
+  };
+
+  const closeReview = () => {
+    setReviewModal(false);
+  };
+
+  const EditUser = () => {
+    setEditUserModal(true);
+  }
+
+  const closeEditUserModal = () => {
+    setEditUserModal(false);
+  }
+
+  const UserInformation = () => {
+    setUserInformationModal(true);
+  }
+
+  const closeUserInformationModal = () => {
+    setUserInformationModal(false);
+  }
 
   return (
     <section className="sidebar">
@@ -70,7 +100,7 @@ function Sidebar() {
         </div>
       </div>
 
-      {/* 로그인 모달 */}
+        {/* 로그인 모달 */}
       {showModal && (
         <LoginModal
           onClose={closeModal}
@@ -79,11 +109,47 @@ function Sidebar() {
             closeModal();
             Singview();
           }}
+          reviewClick={() => {
+            closeModal();
+            Review();}}
+
+            EditUserClick={() => {
+              closeModal();
+              EditUser();}}
+
+              UserInformationClick={() => {
+                closeModal();
+                UserInformation();}}
         />
       )}
 
       {/* 회원가입 모달 */}
       {showSignModal && <SignupModal onClose={closeSingModal} />}
+
+      {/* 리뷰 모달 */}
+      {showReviewModal && <ReviewModal onClose={closeReview} />}
+
+      {/* 유저 에디트 모달 */}
+      {showEditUserModal && (
+  <EditUserModal
+    onClose={closeEditUserModal}
+    UserInformationClick={() => {
+      closeEditUserModal();
+      UserInformation();
+    }}
+  />
+)}
+
+     {/* 유저 프로필 모달 */}
+      {showUserInformationModal && (
+        <UserInformationModal
+          onClose={closeUserInformationModal}
+          UserInformationClick={() => {
+            closeUserInformationModal();
+            EditUser();
+          }}
+        />
+      )}
 
       <div className={`sideOpen ${open ? "open" : "closed"}`}>
         <SideOpen
